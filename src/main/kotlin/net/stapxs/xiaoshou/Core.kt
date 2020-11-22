5e9c0584a6386a4877469e4cbb1b4898725d770b
Stapx Steve <1007028430.stapx@gmail.com>
2020-11-22 23:43:10 +0800

    Dev-0.3.1 | 你好……世界，数据和存储。

--- src/main/kotlin/net/stapxs/xiaoshou/Core.kt
+++ src/main/kotlin/net/stapxs/xiaoshou/Core.kt
@@ -1,5 +1,31 @@
 package net.stapxs.xiaoshou
 
-class Core {
+import com.google.gson.Gson
+import net.stapxs.xiaoshou.features.Log
+import net.stapxs.xiaoshou.features.Options
+import java.util.*
+import kotlin.system.exitProcess
 
+// 版本号
+const val sysVersion = "Dev-0.3.1"
+// 全局 Json 反序列化 Gson
+val gson = Gson()
+// 全局 IO
+val reader = Scanner(System.`in`)
+// 退出标识
+var exit = false
+
+
+suspend fun main() {
+    // 初始化日志输出
+    Log.logSaver()
+    Log.addLog("log", "Log 输出系统初始化完成！")
+
+    // 初始化设置
+    if(!Options.readOpt()) {
+        // 设置文件不存在
+        println("ERR >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> 设置文件不存在。")
+        exit = true
+        exitProcess(-1)
+    }
 }
--- src/main/kotlin/net/stapxs/xiaoshou/features/Options.kt
+++ src/main/kotlin/net/stapxs/xiaoshou/features/Options.kt
@@ -1,5 +1,106 @@
 package net.stapxs.xiaoshou.features
 
-class Options {
-    
+import java.io.File
+
+object Options {
+
+    class OptVer(name:String, value:String) {
+        val optName:String = name
+        var optValue:String = value
+
+    }
+    var optList:MutableList<OptVer> = mutableListOf()
+
+    /**
+     * @Author Stapxs
+     * @Description //TODO 初始化读取设置
+     * @Date 下午 08:09 2020/11/22
+     * @Param
+     * @return
+     **/
+    fun readOpt(): Boolean {
+        println("> 开始初始化设置系统……")
+        try {
+            if(!File("Options.ini").exists()) {
+                // 未找到设置文件……
+                return  false
+            }
+            val optFileList: List<String> = File("Options.ini").readLines()
+            println(">> 读取到如下设置：")
+            for (str in optFileList) {
+                if(str.substring(0, 2) != "##") {
+                    val strList:List<String> = mutableListOf(
+                        str.substring(0, str.indexOf(":")),
+                        str.substring(str.indexOf(":") + 1)
+                    )
+                    val opt = OptVer(strList[0], strList[1])
+                    optList.add(opt)
+                    if(opt.optName != "qqPassword") {
+                        println("\t" + opt.optName + " : " + opt.optValue)
+                    }
+                }
+            }
+            return true
+        }
+        catch (e: Throwable) {
+            return  false
+        }
+    }
+
+    fun getOpt(name:String): String {
+        for(opt in optList) {
+            if(opt.optName == name) {
+                return opt.optValue
+            }
+        }
+        return "Err " + "没有找到这个设置项"
+    }
+
+    /**
+     * @Author Stapxs
+     * @Description //TODO 更改设置（自动保存）
+     * @Date 下午 08:12 2020/11/22
+     * @Param
+     * @return
+     **/
+    fun setOpt(name:String, value: String):Boolean {
+        return try {
+            var isChanged = false
+            for (opt in optList) {
+                if (opt.optName == name) {
+                    opt.optValue = value
+                    isChanged = true
+                }
+            }
+            if(!isChanged) {
+                val opt = OptVer(name, value)
+                optList.add(opt)
+            }
+            saveOpt()
+            true
+        }
+        catch (e: Throwable) {
+            false
+        }
+    }
+
+    /**
+     * @Author Stapxs
+     * @Description //TODO 保存设置
+     * @Date 下午 08:12 2020/11/22
+     * @Param
+     * @return
+     **/
+    private fun saveOpt():String {
+        try {
+            File("Options.ini").writeText("")
+            for (opt in optList) {
+                File("Options.ini").appendText(opt.optName + ":" + opt.optValue + "\n")
+            }
+        }
+        catch (e: Throwable) {
+            return "err " + e.message
+        }
+        return "err " + "未知错误 > Options.tk"
+    }
 }
